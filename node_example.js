0afbc87ab24afab14618e6984e7b1a7c3292192e
allierowan <allie.rowan@mapbox.com>
2018-06-01 16:03:05 -0400

    pass through input in binding

--- node_example.js
+++ node_example.js
@@ -1,3 +1,3 @@
 var valhalla = require('.');
 
-console.log('hello', valhalla.route())
+console.log('hello', valhalla.route('{"locations":[{"lat":40.546115,"lon":-76.385076,"type":"break"}, {"lat":40.544232,"lon":-76.385752,"type":"break"}],"costing":"auto"}'));
